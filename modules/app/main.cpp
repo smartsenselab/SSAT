2edd97ef7b1f07680dba2e4d7a993019fb3d30a7
acnazarejr <acnazarejr@gmail.com>
2016-09-01 16:32:24 -0300

    First commit

---
+++ modules/app/main.cpp
@@ -0,0 +1,19 @@
+#include <QApplication>
+#include "mainwindow.h"
+
+#include <QCoreApplication>
+
+#include <stdlib.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    MainWindow mainWindow;
+    mainWindow.show();
+    return app.exec();
+}
