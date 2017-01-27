949f1a29a1a75dc3273755dd13dca8b0858fc237
brenocg29 <gabrichbreno@gmail.com>
2017-01-27 12:20:07 -0200

    comments

--- modules/app/qpoweroftwospinbox.h
+++ modules/app/qpoweroftwospinbox.h
@@ -8,9 +8,17 @@ class QPowerOfTwoSpinBox : public QSpinBox
     Q_OBJECT
 
 public:
+    ///
+    /// \brief QPowerOfTwoSpinBox Contructor for a spinbox
+    /// \param parent parent of the spinbox
+    ///
     QPowerOfTwoSpinBox(QWidget* parent = 0);
 
 public slots:
+    ///
+    /// \brief stepBy Change the number selected in the spinbox.
+    /// \param steps number of jumps in the spinbox alway two by two
+    ///
     void stepBy(int steps);
 };
 
