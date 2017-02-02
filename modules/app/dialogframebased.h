4a98dcbebc434deddbb3d1f71f122c3fcf00bccb
davibeltrao <davibeltrao02@gmail.com>
2017-02-02 15:44:32 -0200

    Code A commented

--- modules/app/dialogframebased.h
+++ modules/app/dialogframebased.h
@@ -33,39 +33,121 @@ private:
 
 public:
     explicit DialogFrameBased(QWidget *parent = 0);
+    ///
+    /// \brief getIniFrameValue return the Ini Frame value
+    ///
     int getIniFrameValue();
+    ///
+    /// \brief getEndFrameValue return the End Frame value
+    ///
     int getEndFrameValue();
+    ///
+    /// \brief getInfoValue return the Info string
+    ///
     QString getInfoValue();
 
 private:
+    ///
+    /// \brief connectSignalSlots Connect button signals
+    ///
     void connectSignalSlots();
+    ///
+    /// \brief initializeComboboxes Load all categories and labels from Core and populate the ComboBoxes
+    ///
     void initializeComboboxes();
-
+    ///
+    /// \brief enableDisableButtonBox Enable Ok/Cancel button and set FinalFrame equals to IniFrame
+    ///
     void enableDisableButtonBox();
 
 public slots:
+    ///
+    /// \brief slot_initializeDialog Disable buttons and initialize variables
+    /// \param _singleton Core
+    /// \param _frameId actual frame
+    ///
     void slot_initializeDialog(Core &_singleton, const int _frameId);
+    ///
+    /// \brief slot_initializeDialog Initialize the FrameBased variables with the value
+    ///        of the previous item inserted in table
+    /// \param _singleton Core
+    /// \param _index index of the row selected(row to change the value)
+    ///
     void slot_initializeDialog(Core &_singleton, const QModelIndex _index);
+    ///
+    /// \brief slot_comboBoxCategoryActivated Load all labels of the current category selected
+    /// \param _text Current Category
+    ///
     void slot_comboBoxCategoryActivated(const QString &_text);
-
+    ///
+    /// \brief slot_rewindButtonPressed Change the FinalFrame value to the new frameId
+    ///
     void slot_rewindButtonPressed();
+    ///
+    /// \brief slot_backButtonPressed Change the FinalFrame value to the new frameId
+    ///
     void slot_backButtonPressed();
+    ///
+    /// \brief slot_forwardButtonPressed Change the FinalFrame value to the new frameId
+    ///
     void slot_forwardButtonPressed();
+    ///
+    /// \brief slot_fastfButtonPressed Change the FinalFrame value to the new frameId
+    ///
     void slot_fastfButtonPressed();
+    ///
+    /// \brief slot_buttonBoxAccepted update core with FrameBased values
+    ///
     void slot_buttonBoxAccepted();
+    ///
+    /// \brief slot_buttonBoxRejected Discard all FrameBased values
+    ///
     void slot_buttonBoxRejected();
+    ///
+    /// \brief slot_spinBoxValueChanged Enable SpinBoxButton and set the minimum values
+    ///
     void slot_spinBoxValueChanged();
+    ///
+    /// \brief slot_lineEditInfoChanged ?Chama a mesma funçao que o spinboxvalue?
+    ///
     void slot_lineEditInfoChanged();
 
 signals:
+    ///
+    /// \brief signal_rewindButtonPressed Signal to call Slot_rewind
+    ///
     void signal_rewindButtonPressed();
+    ///
+    /// \brief signal_backButtonPressed Signal to call Slot_back
+    ///
     void signal_backButtonPressed();
+    ///
+    /// \brief signal_forwardButtonPressed Signal to call Slot_foward
+    ///
     void signal_forwardButtonPressed();
+    ///
+    /// \brief signal_fastfButtonPressed signal to call Slot_fastf
+    ///
     void signal_fastfButtonPressed();
+    ///
+    /// \brief signal_buttonBoxAccepted Signal to call buttonBoxAccpeted
+    ///
     void signal_buttonBoxAccepted();
 
+    ///
+    /// \brief signal_frameBasedInsertAccepted Signal emited when a new data will be inserted
+    /// \param _data New FrameBased value to be inserted
+    ///
     void signal_frameBasedInsertAccepted(const FrameBasedData _data);
+    ///
+    /// \brief signal_frameBasedAlterAccepted Signal emited when a old data values will be changed
+    /// \param _data FrameBased Value to be changed
+    /// \param _index Row index
+    ///
     void signal_frameBasedAlterAccepted(const FrameBasedData _data, const int _index);
+    ///
+    /// \brief signal_frameBasedRejected ?
+    ///
     void signal_frameBasedRejected();
 };
 
--- modules/core/boundingbox.h
+++ modules/core/boundingbox.h
@@ -16,25 +16,59 @@ using cv::Rect;
 class BoundingBox : public Data
 {
 public:
+    ///
+    /// \brief BoundingBox boundingbox constructor
+    ///
     BoundingBox();
     BoundingBox(const BoundingBox &_box);
     BoundingBox(const Rect &_box);
     BoundingBox(const int _x, const int _y,
                 const int _w, const int _h);
 
+    /// \brief getX Get the x cordinate of the boundingbox
+    ///
     int getX() const;
+    ///
+    /// \brief getY Get the y cordinate of the boudingbox
+    ///
     int getY() const;
+    ///
+    /// \brief getW Get the boundingbox Width
+    ///
     int getW() const;
+    ///
+    /// \brief getH Get the boudingbox Heigth
+    ///
     int getH() const;
 
+    ///
+    /// \brief setX Set the boundingbox's x cordinate
+    /// \param _x New value of x
+    ///
     void setX(const int _x);
+    ///
+    /// \brief setY Set the boundingbox's y coordinate
+    /// \param _y New value of y
+    ///
     void setY(const int _y);
+    ///
+    /// \brief setW Set the boundingbox width
+    /// \param _w New value of width
+    ///
     void setW(const int _w);
+    ///
+    /// \brief setH Set the boundingbox heigth
+    /// \param _h New value of heigth
+    ///
     void setH(const int _h);
-
+    ///
+    /// \brief operator = Update boundingbox values(attributes, cordinates, comments, id, name)
+    /// \param _box Updated BoudingBox values
+    ///
     void operator=(const BoundingBox &_box);
 
 private:
+    ///boundingbox coordinates(x,y,width,heigth)
     Rect coordinates;
 };
 
