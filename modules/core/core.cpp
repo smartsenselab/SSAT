7a5cc8345919c6297ff7aca168116706d3a3f9f0
Rafael Vareto <rafaelvareto@gmail.com>
2017-08-20 21:43:10 -0300

    Removing demo data

--- modules/core/core.cpp
+++ modules/core/core.cpp
@@ -4,28 +4,6 @@ Core::Core(unsigned int _frames, unsigned int _cores)
 {
     this->frames.resize(_frames);
     this->pool = new ThreadPool(_cores);
-
-//    this->attributes.insert(std::pair<string, string>("Object_recognition", "Table"));
-//    this->attributes.insert(std::pair<string, string>("Object_recognition", "Tennis"));
-//    this->attributes.insert(std::pair<string, string>("Object_recognition", "Desk"));
-//    this->attributes.insert(std::pair<string, string>("Object_recognition", "Television"));
-
-//    this->attributes.insert(std::pair<string, string>("Person_identification", "Breno"));
-//    this->attributes.insert(std::pair<string, string>("Person_identification", "Danilo"));
-//    this->attributes.insert(std::pair<string, string>("Person_identification", "Davi"));
-//    this->attributes.insert(std::pair<string, string>("Person_identification", "Rafael"));
-
-
-//    std::multimap<string,string>::iterator it;
-//    for(it = this->attributes.begin(); it != this->attributes.end(); it++)
-//    {
-//        for(int index = 0; index < 10; index++)
-//        {
-//            string sIndex = std::to_string(index);
-//            FrameBasedData temp = FrameBasedData(index, index * 4, it->first, it->second, it->second + " " + sIndex);
-//            this->frameData.push_back(temp);
-//        }
-//    }
 }
 
 Core::~Core()
