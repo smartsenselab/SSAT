99fe94ea651289cf144b54400b00e919ba3c74a9
Rafael Vareto <rafaelvareto@gmail.com>
2018-06-19 13:20:24 -0300

    Implementing new spinBoxSpeeds

--- modules/app/qpoweroftwospinbox.cpp
+++ modules/app/qpoweroftwospinbox.cpp
@@ -2,18 +2,34 @@
 
 QPowerOfTwoSpinBox::QPowerOfTwoSpinBox(QWidget* parent) : QSpinBox(parent)
 {
-
+    this->setMinimum(1);
+    this->setMaximum(128);
+    QSpinBox::setMaximum(128);
 }
 
 void QPowerOfTwoSpinBox::stepBy(int steps)
 {
     if(steps == 1)
     {
-        this->setValue(this->value() * 2);
+        if (this->value() < 10)
+        {
+            this->setValue(this->value() + 1);
+        }
+        else
+        {
+            this->setValue(this->value() + 10);
+        }
     }
     else if(steps == -1)
     {
-        this->setValue(this->value() / 2);
+        if (this->value() <= 10)
+        {
+            this->setValue(this->value() - 1);
+        }
+        else
+        {
+            this->setValue(this->value() - 10);
+        }
     }
     else
     {
