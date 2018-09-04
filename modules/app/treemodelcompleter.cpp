0987d600537160677a3dfee789202848d66d11ff
Rafael Vareto <rafaelvareto@gmail.com>
2018-09-04 17:59:25 -0300

    Adding TreeModelCompleter class

---
+++ modules/app/treemodelcompleter.cpp
@@ -0,0 +1,47 @@
+#include "treemodelcompleter.h"
+#include <QStringList>
+
+TreeModelCompleter::TreeModelCompleter(QObject *parent)
+    : QCompleter(parent)
+{
+}
+
+TreeModelCompleter::TreeModelCompleter(QAbstractItemModel *model, QObject *parent)
+    : QCompleter(model, parent)
+{
+}
+
+void TreeModelCompleter::setSeparator(const QString &separator)
+{
+    this->sep = separator;
+}
+
+QString TreeModelCompleter::separator() const
+{
+    return this->sep;
+}
+
+QStringList TreeModelCompleter::splitPath(const QString &path) const
+{
+    if (this->sep.isNull()) {
+        return QCompleter::splitPath(path);
+    }
+
+    return path.split(this->sep);
+}
+
+QString TreeModelCompleter::pathFromIndex(const QModelIndex &index) const
+{
+    if (this->sep.isNull()) {
+        return QCompleter::pathFromIndex(index);
+    }
+
+    // navigate up and accumulate data
+    QStringList dataList;
+    for (QModelIndex i = index; i.isValid(); i = i.parent())
+    {
+        dataList.prepend(model()->data(i, completionRole()).toString());
+    }
+
+    return dataList.join(this->sep);
+}
